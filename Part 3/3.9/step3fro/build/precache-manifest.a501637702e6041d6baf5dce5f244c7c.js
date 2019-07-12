c23bc2d7146dc43c6040007d30dbd9e1a3884332
girmsam <51339253+girmsam@users.noreply.github.com>
2019-07-12 14:03:09 +0300

    Add files via upload

---
+++ Part 3/3.9/step3fro/build/precache-manifest.a501637702e6041d6baf5dce5f244c7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5349b843179eb1caff749a7b12e27f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf9626f3b70f7d2bd2c8",
+    "url": "/static/css/main.2a926d45.chunk.css"
+  },
+  {
+    "revision": "74c5f32d9465ae1855df",
+    "url": "/static/js/2.fb97593d.chunk.js"
+  },
+  {
+    "revision": "bf9626f3b70f7d2bd2c8",
+    "url": "/static/js/main.a801a45a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
